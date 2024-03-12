c860cb0e489a02307b1e736be04c7bd0d5402fb9
Adri1P <adrien.peccenini@student-cs.fr>
2024-03-12 14:52:52 +0100

    test1 nexttrack

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import logo from './assets/logo.svg';
 import './App.css';
+import {fetchTracks} from './lib/fetchTracks'
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+
+
+
+const [trackIndex, setTrackIndex] = useState(0)
+
+const goToNextTrack = () => {
+  setTrackIndex(trackIndex + 1);
+}
 
 const trackUrls = [
   'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
@@ -9,7 +20,10 @@ const trackUrls = [
   'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
 ];
 
-
+const { data: tracks } = useQuery({
+  queryKey: ['tracks'],
+  queryFn: fetchTracks
+});
 
 const App = () => {
   return (
@@ -22,10 +36,16 @@ const App = () => {
         <p>Je vais modifier le code pour faire un vrai blind test !</p>
       </div>
       <div className="App-buttons"></div>
-      <audio src={trackUrls[1]} autoPlay controls />
+      <audio src={trackUrls[trackIndex]} autoPlay controls />
+      <button onClick={goToNextTrack}>
+      Next track
+      </button>
     </div>
   );
 };
+
+
+
 
 
 export default App;
--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,4 +1,5 @@
-const apiToken: string = '';
+
+const apiToken: string = 'BQC6otvIg2GnkcS9f3doiOOOK3Gye73kgLkKL4BsYB8kwD5YdgMZXTrjzrqeapAzzCwEKYbwWcHORTHYhWqr6pV-DUf4-I3-JCwRRI-JvWbXQ9YgtrupM_yyq_4V5cPtyesZbQzUv68dUj_S4gBJLHkIVklfcCPgqY1Xl8kTUlPTq_bGNqqATK_o-Lwz-olT6HerXJshBvglJllibds';
 
 export const fetchTracks = async () => {
   const response = await fetch('https://api.spotify.com/v1/me/tracks', {
